47e5bc76a9ae6e66839ff3d7b16aab1f7b0d60ea
Erfan <erfanmoghadamy13805@gmail.com>
2026-08-11 10:53:55 +0330

    feat: add product domain entity

---
+++ ProductCatalog.Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace ProductCatalog.Domain.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public string Description { get; set; } = null!;
+
+    public int Stock { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+}
