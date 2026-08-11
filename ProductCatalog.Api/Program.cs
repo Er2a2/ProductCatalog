17229ad0eb862734e594521c0cd4cb5660636095
Erfan <erfanmoghadamy13805@gmail.com>
2026-08-11 12:28:26 +0330

    feat: configure redis caching infrastructure

--- ProductCatalog.Api/Program.cs
+++ ProductCatalog.Api/Program.cs
@@ -1,8 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using ProductCatalog.Application.Interfaces.Caching;
 using ProductCatalog.Application.Interfaces.Products;
 using ProductCatalog.Application.Services.Products;
+using ProductCatalog.Infrastructure.Caching;
 using ProductCatalog.Infrastructure.Persistence;
 using ProductCatalog.Infrastructure.Persistence.Repositories;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,14 +16,22 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//RegisterSQL
 builder.Services.AddDbContext<ProductCatalogDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
 
+//Register Redis
+var redisConnectionString =
+    builder.Configuration["Redis:ConnectionString"];
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(
+    ConnectionMultiplexer.Connect(redisConnectionString!));
+
 //Register DI
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductService, ProductService>();
-
+builder.Services.AddScoped<ICacheService, RedisCacheService>();
 
 var app = builder.Build();
 
