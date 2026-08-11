8d6fa7e87bc4ac090c69155fff13deb17b00d1c9
Erfan <erfanmoghadamy13805@gmail.com>
2026-08-11 11:01:56 +0330

    feat: configure product persistence

---
+++ ProductCatalog.Infrastructure/Persistence/ProductCatalogDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalog.Domain.Entities;
+
+namespace ProductCatalog.Infrastructure.Persistence;
+
+public class ProductCatalogDbContext : DbContext
+{
+    public ProductCatalogDbContext(DbContextOptions<ProductCatalogDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products => Set<Product>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(ProductCatalogDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
